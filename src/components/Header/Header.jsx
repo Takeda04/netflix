e31596257de322597f718b3b2a1f54847bf23094
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-04-03 22:56:23 +0500

    fin

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
   return (
     <div>
       <header>
-        <nav className="bg-white border-gray-200 px-4 py-6  lg:px-6 py-2.5 dark:bg-black">
+        <nav className="bg-white border-gray-200 px-4 py-6  lg:px-6 dark:bg-black">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="https://flowbite.com" className="flex items-center">
               <img
