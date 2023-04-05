a1f76cf4081b17c62b0089eff9853049bd8c3a4e
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-04-05 22:44:29 +0500

    salom

--- src/components/Card/MainCard.jsx
+++ src/components/Card/MainCard.jsx
@@ -3,9 +3,12 @@ import { IMG_URL } from "../../utils/Api"
 const MainCard = ({item}) => {
   return (
     <div className='w-[300px]  mb-4 card-body text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110'>
-			<a href={item.link} target={'_blank'}>
-              <img className="" src={`${IMG_URL}${item.backdrop_path}`} alt='slaom' />
-            </a>
+      
+			<button className="border-0">
+        <img className="" src={`${IMG_URL}${item.backdrop_path}`} alt='slaom' />
+        <h3 className="text-white mt-2">{item.title}</h3>
+        <p className="text-slate-500 mt-2">{item.release_date}</p>
+      </button>
 		</div>
   )
 }
--- src/components/Slider/MainSlider.jsx
+++ src/components/Slider/MainSlider.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BASE_URL2 } from "../../utils/Api";
+import { BASE_URL4 } from "../../utils/Api";
 import ky from "ky";
 import MainCard from "../Card/MainCard";
 
-const films = await ky.get(BASE_URL2).json();
+const films = await ky.get(BASE_URL4).json();
  const movies = films.results;
+ console.log(movies);
 export default class SimpleSlider extends Component {
     state = {
         movies: []
@@ -23,16 +24,13 @@ export default class SimpleSlider extends Component {
             autoplay: true
             
         };
-        console.log(settings);
         return (
             <div className="px-[50px] mt-[680px]">
-
+                <h2 className="text-white text-2xl mb-4">Netflix Orijinal İçerikleri</h2>
                 <Slider {...settings} className="">
                          {movies.map((item, i) => (
                            <MainCard key={i} item={item}/>
                          ))}
-                  <div></div>
-                  <div></div>
                 </Slider>
             </div>
         );
